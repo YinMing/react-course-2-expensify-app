dca1095060173848521c7899e3969986d4044bf3
YinMing <apluskei@gmail.com>
2018-05-22 22:13:41 +0800

    setup devDependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,77 +1,49 @@
 const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = (env)=>{
-    const isProduction = env === 'production';
-    const CSSExtract = new ExtractTextPlugin('styles.css');
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
 
-
-    return {
-        entry: './src/app.js',
-        output: {
-            path: path.join(__dirname, 'public'),
-            filename: 'bundle.js'
-        },
-        module:{
-            rules:[{
-                loader: 'babel-loader',
-                test: /\.js$/,
-                exclude: /node_modules/
-            }, {
-                test: /\.s?css$/,
-                use: CSSExtract.extract({
-                    use: [
-                            {
-                                loader: 'css-loader',
-                                options:{
-                                    sourceMap: true
-                            }},{
-                                loader: 'sass-loader',
-                                options:{
-                                    sourceMap: true
-                                }
-                            }]
-                })
-            }]
-        },
-        plugins: [
-           CSSExtract
-        ],
-        devtool: isProduction ? 'source-map' : 'inline-source-map',
-        devServer: {
-            contentBase: path.join(__dirname, 'public'),
-            historyApiFallback: true //tell server handle client-side routing
-        }
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public', 'dist'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        use: CSSExtract.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
+      }]
+    },
+    plugins: [
+      CSSExtract
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+      publicPath: '/dist/'
     }
-}
-
-//https://webpack.js.org/
-//https://nodejs.org/api/path.html#path_path_join_paths
-// module.exports = {
-//     entry: './src/app.js',
-//     output: {
-//         path: path.join(__dirname, 'public'),
-//         filename: 'bundle.js'
-//     },
-//     module:{
-//         rules:[{
-//             loader: 'babel-loader',
-//             test: /\.js$/,
-//             exclude: /node_modules/
-//         }, {
-//             test: /\.s?css$/,
-//             use: [
-//                 'style-loader',
-//                 'css-loader',
-//                 'sass-loader'
-//             ]
-//         }]
-//     },
-//     devtool: 'cheap-module-eval-source-map',
-//     devServer: {
-//         contentBase: path.join(__dirname, 'public'),
-//         historyApiFallback: true //tell server handle client-side routing
-//     }
-// }
-
-//loader, customize the behavior of webpack
+  };
+};
